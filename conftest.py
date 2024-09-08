c47fb535e5706b87f31d5492364f435c4c1b56ae
lebedeva.daria <lebedeva.daria@mx.mdcm.co>
2024-09-08 21:17:45 +0600

    Logging works

--- conftest.py
+++ conftest.py
@@ -1,10 +1,20 @@
 import pytest
 import logging
+import logging.config
 from playwright.sync_api import sync_playwright
+from pages.random_org_pages import RandomOrgPage
+
+logging.config.fileConfig('settings/logging. conf')
+logger = logging.getLogger()
 
 @pytest.fixture(scope="module", params=["chromium", "firefox", "webkit"])
 def browser(request):
+    """
+    Фикстура для запуска браузера. Поддерживает Chromium, Firefox и WebKit
+    Логирует запуск и закрытие браузера
+    """
     with sync_playwright() as p:
+        logger.info(f"Запуск браузера: {request.param}")
         if request.param == "chromium":
             browser = p.chromium.launch(headless=False)
         elif request.param == "firefox":
@@ -14,14 +24,28 @@ def browser(request):
         else:
             raise ValueError(f"Неподдерживаемый браузер: {request.param}")
         yield browser
+        logger.info(f"Закрытие браузера: {request.param}")
         browser.close()
 
 @pytest.fixture(scope="module")
 def page(browser):
+    """
+    Фикстура для создания новой страницы в контексте браузера
+    Логирует открытие страницы
+    """
     context = browser.new_context()
     page = context.new_page()
-    page.goto("https://www.random.org/")
+    logger.info("Открытие страницы https://www.random.org/widgets/integers/iframe")
+    page.goto("https://www.random.org/widgets/integers/iframe")
     # Установить куки
     # page.context.add_cookies([{"name": "cookie_name", "value": "cookie_value", "domain": ".random.org"}])
     yield page
     context.close()
+
+
+@pytest.fixture(scope="module")
+def random_org_page(page):
+    """
+    Фикстура для создания объекта страницы RandomOrgPage
+    """
+    return RandomOrgPage(page)
